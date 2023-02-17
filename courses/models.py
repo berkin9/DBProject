c12e1e1aaec1fee9cb02c349f236a0a199788a7e
Berkin <ab7675@jamk.student.fi>
2023-02-17 17:36:07 +0300

    new

--- courses/models.py
+++ courses/models.py
@@ -7,7 +7,15 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date= models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",null=False, unique=True, db_index=True) 
+    slug = models.SlugField(default="",null=False,blank=True, unique=True, db_index=True)
+    type = models.TextField(default="empty",null=False)
+
+    TYPE_CHOICES = (
+        ("programlama", "programlama"),
+        ("web-gelistirme", "web-gelistirme"),
+        ("mobil-gelistirme", "mobil-gelistirme"),
+    )
+    type = models.CharField(max_length = 20, choices = TYPE_CHOICES, default = 'empty', null=False,blank=False)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
--- courses/views.py
+++ courses/views.py
@@ -4,48 +4,6 @@ from django.http import Http404, HttpResponseNotFound
 from django.urls import reverse
 from .models import Course, Category
 
-data = {
-    "programlama":"programlama kategorisine ait kurslar",
-    "web-gelistirme":"web gelistirme kategorisine ait kurslar",
-    "mobil":"mobil kategorisine ait kurslar",
-}
-
-db = {
-    "courses": [
-        {
-            "title":"javascript kursu",
-            "description":"javascript kurs açıklaması",
-            "imageUrl": "1.jpg",
-            "slug": "javascript-kursu",
-            "date":datetime.now(),
-            "isActive": True,
-            "isUpdated": False
-        },
-        {
-            "title":"python kursu",
-            "description":"python kurs açıklaması",
-            "imageUrl": "2.jpg",
-            "slug": "python-kursu",
-            "date": date(2022,9,10),
-            "isActive": False,
-            "isUpdated": False
-        },
-        {
-            "title":"web geliştirme kursu",
-            "description":"web geliştirme kurs açıklaması",
-            "imageUrl": "3.jpg",
-            "slug": "web-gelistirme-kursu",
-            "date": date(2022,8,10),
-            "isActive": True,
-            "isUpdated": True
-        }
-    ],
-    "categories": [
-        { "id": 1, "name": "programlama", "slug":"programlama" },
-        { "id": 2, "name": "web geliştirme", "slug":"web-gelistirme" },
-        { "id": 3, "name": "mobil uygulamalar", "slug":"mobil-uygulamalar" },
-    ]
-}
 
 def index(request):
     kurslar = Course.objects.filter(isActive=1)
@@ -55,6 +13,7 @@ def index(request):
         'categories': kategoriler,
         'courses': kurslar
     })
+    
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
@@ -66,10 +25,10 @@ def details(request, slug):
 
 def getCoursesByCategory(request, category_name):
     try:
-        category_text = data[category_name];    
+        kurslar = Course.objects.filter(type = category_name, isActive=1) 
         return render(request, 'courses/kurslar.html', {
             'category': category_name,
-            'category_text': category_text 
+            'courses': kurslar 
         })
     except:
         return HttpResponseNotFound("<h1>yanlış kategori seçimi</h1>")
